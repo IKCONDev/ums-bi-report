5ba9a618c8e9f0ae826d36281b036ca937637c89
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-11-01 16:52:21 +0530

    Initial Commit

---
+++ src/main/java/com/ikn/ums/bi/report/UmsBiReportApplication.java
@@ -0,0 +1,13 @@
+package com.ikn.ums.bi.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsBiReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsBiReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikn/ums/bi/report/UmsBiReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikn.ums.bi.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsBiReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
