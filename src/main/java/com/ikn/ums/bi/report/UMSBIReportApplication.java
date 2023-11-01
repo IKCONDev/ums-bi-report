dea01724a558008a352d2a619ecd3c7fd604dc49
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-11-01 17:00:26 +0530

    renamed main class

--- src/main/java/com/ikn/ums/bi/report/UMSBIReportApplication.java
+++ src/main/java/com/ikn/ums/bi/report/UMSBIReportApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UmsBiReportApplication {
+public class UMSBIReportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UmsBiReportApplication.class, args);
+		SpringApplication.run(UMSBIReportApplication.class, args);
 	}
 
 }
