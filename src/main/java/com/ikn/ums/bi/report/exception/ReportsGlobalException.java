ee4d65f9ef29e269efd376b9841c645b8af0f8ae
MaheshJarati <MaheshJarati@IKCON1019>
2023-11-06 15:22:26 +0530

    Initial implementation

---
+++ src/main/java/com/ikn/ums/bi/report/exception/ReportsGlobalException.java
@@ -0,0 +1,48 @@
+package com.ikn.ums.bi.report.exception;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+
+@Component
+public class ReportsGlobalException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String errorCode;
+	private String errorMessage;
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public ReportsGlobalException(String errorCode, String errorMessage) {
+		super(errorMessage , new Throwable(errorCode) );
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+	
+	public ReportsGlobalException() {
+		
+	}
+
